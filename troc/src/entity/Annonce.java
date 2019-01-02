e785adc95e950f62fdf689946c196fdf3395565f
karimbouhnek <karimbouhnek@gmail.com>
2019-01-02 12:13:48 +0100

    Commande commit 75%

---
+++ troc/src/entity/Annonce.java
@@ -0,0 +1,116 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+/**
+ *
+ * @author Choubi
+ */
+public class Annonce 
+{
+    private int id;
+    private String titre;
+    private float prix;
+    private String type;
+    private String etat;
+    private String adress;
+    private String description;
+    private int idCat;
+    private int idSousCat;
+    private int idUser;
+
+    public Annonce() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+
+    public float getPrix() {
+        return prix;
+    }
+
+    public void setPrix(float prix) {
+        this.prix = prix;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getEtat() {
+        return etat;
+    }
+
+    public void setEtat(String etat) {
+        this.etat = etat;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getIdCat() {
+        return idCat;
+    }
+
+    public void setIdCat(int idCat) {
+        this.idCat = idCat;
+    }
+
+    public int getIdSousCat() {
+        return idSousCat;
+    }
+
+    public void setIdSousCat(int idSousCat) {
+        this.idSousCat = idSousCat;
+    }
+
+    public int getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(int idUser) {
+        this.idUser = idUser;
+    }
+
+    @Override
+    public String toString() {
+        return "Annonce{" + "id=" + id + ", titre=" + titre + ", prix=" + prix + ", type=" + type + ", etat=" + etat + ", adress=" + adress + ", description=" + description + ", idCat=" + idCat + ", idSousCat=" + idSousCat + ", idUser=" + idUser + '}';
+    }
+
+   
+    
+    
+}
